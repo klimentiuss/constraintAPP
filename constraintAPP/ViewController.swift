6707b01b80147c6200015e09516663e057bf3d65
Danil <klimenko.apple01@icloud.com>
2022-02-15 13:21:27 +0200

    redYellowGreen is done

--- constraintAPP/ViewController.swift
+++ constraintAPP/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    enum CurrentLight {
+        case red, yellow, green
     }
 
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        redView.alpha = lightIsOff
+        yellowView.alpha = lightIsOff
+        greenView.alpha = lightIsOff
+        
+        redView.layer.cornerRadius = 100
+        yellowView.layer.cornerRadius = 100
+        greenView.layer.cornerRadius = 100
+    }
 
+    @IBAction func startButtonPressed() {
+        
+        
+        startButton.setTitle("Next", for: .normal)
+        
+        
+        switch currentLight {
+        case .red:
+            greenView.alpha = lightIsOff
+            redView.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redView.alpha = lightIsOff
+            yellowView.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowView.alpha = lightIsOff
+            greenView.alpha = lightIsOn
+            currentLight = .red
+        }
+        
+        
+    }
+    
 }
+    
+
 
